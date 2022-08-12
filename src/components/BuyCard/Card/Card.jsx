832d2101e4f1ed41e430356e8cd327a34b28d3dc
Andrei Bobryk <andrei.bobryk@celadonsoft.com>
2022-08-12 18:11:50 +0300

    feat: add calculate data in buy prague coolpass card

--- src/components/BuyCard/Card/Card.jsx
+++ src/components/BuyCard/Card/Card.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import { useSelector } from "react-redux";
 import classes from "./Card.module.css";
 
 function Card(props) {
+  const [countAdult,  setCountAdult] = useState(0)
+  const [countChild,  setCountChild] = useState(0)
   const contentInterface = useSelector(state => state.translation.language);
   const {  CALCULATOR_COMPLETE_BOOKING_BTN, YOUR_PRICE, CALC_ADULT, STUDENT_CHILD, PRICE, BUY_PRAGUE_CARD_COOL_PASS  } = contentInterface;
+  const {priceAdult, priceChild} = props;
+
+
+  const calculateTotalPrice = () => {
+    return priceAdult * countAdult + priceChild * countChild;
+  }
+  
+  const increaseCountAdult = () => {
+    setCountAdult(countAdult+1);
+    calculateTotalPrice()
+
+  };
+  const decreaseCountAdult = () => {
+   if (countAdult)  {
+    setCountAdult(countAdult-1);
+    calculateTotalPrice();
+
+   }
+  };
+  const increaseCountChild = () => {
+    setCountChild(countChild+1);
+    calculateTotalPrice();
+  };
+  const decreaseCountChild = () => {
+    if (countChild) {
+      setCountChild(countChild-1);
+      calculateTotalPrice();
+    }
+  }
   return (
     <div className={classes.Wrapper}>
       <div className={classes.Header}>
@@ -16,30 +47,30 @@ function Card(props) {
           <span className={classes.AdultCalc__Name}>{ CALC_ADULT ? CALC_ADULT : 'Adult' }</span>
           <span className={classes.AdultCalc__Price}>
             <span className={classes.AdultCalc__PriceTitle}>{PRICE}</span>
-            <span className={classes.AdultCalc__PriceSum}>{props.priceAdult} EUR</span>
+            <span className={classes.AdultCalc__PriceSum}>{priceAdult} EUR</span>
           </span>
-          <button className={classes.AdultCalc__Button} style={{marginLeft:'20px'}}>-</button>
+          <button className={classes.AdultCalc__Button} onClick={decreaseCountAdult} style={{marginLeft:'20px'}}>-</button>
           <span
             className={classes.AdultCalc__Count}
             style={{ minWidth: "60px" }}
           >
-            0
+            {countAdult}
           </span>
-          <button className={classes.AdultCalc__Button}>+</button>
+          <button className={classes.AdultCalc__Button} onClick={increaseCountAdult}>+</button>
         </div>
         <div className={classes.ChildCalc}>
           <span className={classes.ChildCalc__Name}>{STUDENT_CHILD}</span>
-          <span className={classes.ChildCalc__PriceSum}>{props.priceChild} EUR</span>
-          <button className={classes.AdultCalc__Button}>-</button>
+          <span className={classes.ChildCalc__PriceSum}>{priceChild} EUR</span>
+          <button className={classes.AdultCalc__Button} onClick={decreaseCountChild} >-</button>
           <span
             className={classes.ChildCalc__Count}
             style={{ minWidth: "60px" }}
           >
-            0
+            {countChild}
           </span>
-          <button className={classes.AdultCalc__Button}>+</button>
+          <button className={classes.AdultCalc__Button} onClick={increaseCountChild}>+</button>
         </div>
-        <div className={classes.PriceTotal}>{YOUR_PRICE}: <span className={classes.PriceTotalSum}>0.00 EUR</span></div>
+        <div className={classes.PriceTotal}>{YOUR_PRICE} <span className={classes.PriceTotalSum}>{calculateTotalPrice()}.00 EUR</span></div>
       </div>
       <div className={classes.Footer}>
         <button className={classes.Button}>{CALCULATOR_COMPLETE_BOOKING_BTN}</button>
